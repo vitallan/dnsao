554022ebe96bf3708736dbdff548dad19cf6aafb
Allan Vital <allan.vital@gmail.com>
2025-12-13 09:23:11 -0300

    small code improvement

--- src/main/java/com/allanvital/dnsao/dns/processor/QueryProcessor.java
+++ src/main/java/com/allanvital/dnsao/dns/processor/QueryProcessor.java
@@ -11,9 +11,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.xbill.DNS.Message;
 
+import java.io.IOException;
 import java.net.InetAddress;
 
 import static com.allanvital.dnsao.infra.AppLoggers.DNS;
+import static com.allanvital.dnsao.utils.ExceptionUtils.findRootCause;
 
 /**
  * @author Allan Vital (https://allanvital.com)
@@ -32,10 +34,10 @@ public class QueryProcessor {
         this.postHandler = dependencies.getPostHandlerFacade();
     }
 
-    public DnsQuery processQuery(InetAddress clientAddress, byte[] data, boolean isInternalQuery) {
+    public DnsQuery processQuery(InetAddress clientAddress, Message clientQuery, boolean isInternalQuery) {
         DnsQueryRequest request = null;
         try {
-            request = preHandler.prepare(clientAddress, data, isInternalQuery);
+            request = preHandler.prepare(clientAddress, clientQuery, isInternalQuery);
         } catch (PreHandlerException e) {
             log.error(e.getMessage());
             return null;
@@ -46,11 +48,26 @@ public class QueryProcessor {
     }
 
     public DnsQuery processInternalQuery(Message message) {
-        return processQuery(InetAddress.getLoopbackAddress(), message.toWire(), true);
+        return processQuery(InetAddress.getLoopbackAddress(), message, true);
     }
 
     public DnsQuery processExternalQuery(InetAddress clientAddress, byte[] data) {
-        return this.processQuery(clientAddress, data, false);
+        Message clientQuery = buildMessage(data);
+        if (clientQuery == null) {
+            return null;
+        }
+        return this.processQuery(clientAddress, clientQuery, false);
+    }
+
+    private Message buildMessage(byte[] rawMessage) {
+        Message clientQuery = null;
+        try {
+            clientQuery = new Message(rawMessage);
+        } catch (IOException e) {
+            Throwable rootCause = findRootCause(e);
+            log.error("Failed to build query for processing: {} - {}", rootCause, rootCause.getMessage());
+        }
+        return clientQuery;
     }
 
 }
