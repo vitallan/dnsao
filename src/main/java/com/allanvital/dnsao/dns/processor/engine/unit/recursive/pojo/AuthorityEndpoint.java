dcc31a1344abbe5a180c170482ef9d221562770a
Allan Vital <allan.vital@gmail.com>
2026-07-18 19:40:14 -0300

    removing recursive resolution code

--- src/main/java/com/allanvital/dnsao/dns/processor/engine/unit/recursive/pojo/AuthorityEndpoint.java
+++
@@ -1,9 +0,0 @@
-package com.allanvital.dnsao.dns.processor.engine.unit.recursive.pojo;
-
-import java.net.InetAddress;
-
-/**
- * @author Allan Vital (https://allanvital.com)
- */
-public record AuthorityEndpoint(String name, InetAddress address, int port) {
-}
